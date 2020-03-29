93caef7a50be0b4fe1f038fd4c9e4cdf1a63dafe
TFprimeWJ <jw.tfprime@gmail.com>
2020-03-29 15:13:23 +0800

    Init

---
+++ fiveinarow/FiveinarowGame.py
@@ -0,0 +1,99 @@
+import sys
+import numpy as np
+
+sys.path.append('..')
+from Game import Game
+from .FiveinarowLogic import Board
+
+
+class FiveinarowGame(Game):
+    """
+    Connect4 Game class implementing the alpha-zero-general Game interface.
+    """
+    square_content = {
+        -1: "◉",
+        +0: "-",
+        +1: "◯"
+    }
+
+    def __init__(self, height=None, width=None, win_length=None, np_pieces=None):
+        Game.__init__(self)
+        self._base_board = Board(height, width, win_length, np_pieces)
+
+    def getInitBoard(self):
+        return self._base_board.np_pieces
+
+    def getBoardSize(self):
+        return (self._base_board.height, self._base_board.width)
+
+    def getActionSize(self):
+        return self._base_board.width * self._base_board.height
+
+    def getNextState(self, board, player, action):
+        """Returns a copy of the board with updated move, original board is unmodified."""
+        b = self._base_board.with_np_pieces(np_pieces=np.copy(board))
+        move = (int(action/self._base_board.width), action%self._base_board.height)
+        b.add_stone(move, player)
+        return (b.np_pieces, -player)
+
+    def getValidMoves(self, board, player):
+        """Any zero value in the board is a valid move"""
+        return self._base_board.with_np_pieces(np_pieces=board).get_valid_moves().reshape([1,-1]).squeeze()
+
+    def getGameEnded(self, board, player):
+        b = self._base_board.with_np_pieces(np_pieces=board)
+        winstate = b.get_win_state()
+        if winstate.is_ended:
+            if winstate.winner is None:
+                # draw has very little value.
+                return 1e-4
+            elif winstate.winner == player:
+                return +1
+            elif winstate.winner == -player:
+                return -1
+            else:
+                raise ValueError('Unexpected winstate found: ', winstate)
+        else:
+            # 0 used to represent unfinished game.
+            return 0
+
+    def getCanonicalForm(self, board, player):
+        # Flip player from 1 to -1
+        return board * player
+
+    def getSymmetries(self, board, pi):
+        # mirror, rotational
+        assert(len(pi) == self._base_board.width**2)  # 1 for pass
+        pi_board = np.reshape(pi, (self._base_board.width, self._base_board.width))
+        l = []
+
+        for i in range(1, 5):
+            for j in [True, False]:
+                newB = np.rot90(board, i)
+                newPi = np.rot90(pi_board, i)
+                if j:
+                    newB = np.fliplr(newB)
+                    newPi = np.fliplr(newPi)
+                l += [(newB, list(newPi.ravel()) + [pi[-1]])]
+        return l
+
+    def stringRepresentation(self, board):
+        return str(self._base_board.with_np_pieces(np_pieces=board))
+
+
+    @staticmethod
+    def display(board):
+        n = board.shape[0]
+        print("   ", end="")
+        for y in range(n):
+            print(y, end=" ")
+        print("")
+        print("-----------------------")
+        for y in range(n):
+            print(format(y, '<2'), end="")
+            print("|", end="")    # print the row #
+            for x in range(n):
+                piece = board[y][x]    # get the piece to print
+                print(FiveinarowGame.square_content[piece], end=" ")
+            print("|")
+        print("-----------------------")
---
+++ fiveinarow/FiveinarowPlayers.py
@@ -0,0 +1,78 @@
+import numpy as np
+
+
+class RandomPlayer():
+    def __init__(self, game):
+        self.game = game
+
+    def play(self, board):
+        a = np.random.randint(self.game.getActionSize())
+        valids = self.game.getValidMoves(board, 1)
+        while valids[a] != 1:
+            a = np.random.randint(self.game.getActionSize())
+        return a
+
+
+
+class HumanFiveinarowPlayer():
+    def __init__(self, game):
+        self.game = game
+
+    def play(self, board):
+        # display(board)
+        n = self.game.getBoardSize()[0]
+        valid = self.game.getValidMoves(board, 1)
+        # for i in range(len(valid)):
+        #     if valid[i]:
+        #         print("[", int(i/n), int(i%n), end="] ")
+        while True:
+            input_move = input()
+            input_a = input_move.split(" ")
+            if len(input_a) == 2:
+                try:
+                    x,y = [int(i) for i in input_a]
+                    if ((0 <= x) and (x < n) and (0 <= y) and (y < n)):
+                        a = n * x + y
+                        if valid[a]:
+                            break
+                except ValueError:
+                    # Input needs to be an integer
+                    'Invalid integer'
+            print('Invalid move')
+        return a
+
+
+class OneStepLookaheadFiveinarowPlayer():
+    """Simple player who always takes a win if presented, or blocks a loss if obvious, otherwise is random."""
+    def __init__(self, game, verbose=True):
+        self.game = game
+        self.player_num = 1
+        self.verbose = verbose
+
+    def play(self, board):
+        valid_moves = self.game.getValidMoves(board, self.player_num)
+        win_move_set = set()
+        fallback_move_set = set()
+        stop_loss_move_set = set()
+        for move, valid in enumerate(valid_moves):
+            if not valid: continue
+            if self.player_num == self.game.getGameEnded(*self.game.getNextState(board, self.player_num, move)):
+                win_move_set.add(move)
+            if -self.player_num == self.game.getGameEnded(*self.game.getNextState(board, -self.player_num, move)):
+                stop_loss_move_set.add(move)
+            else:
+                fallback_move_set.add(move)
+
+        if len(win_move_set) > 0:
+            ret_move = np.random.choice(list(win_move_set))
+            if self.verbose: print('Playing winning action %s from %s' % (ret_move, win_move_set))
+        elif len(stop_loss_move_set) > 0:
+            ret_move = np.random.choice(list(stop_loss_move_set))
+            if self.verbose: print('Playing loss stopping action %s from %s' % (ret_move, stop_loss_move_set))
+        elif len(fallback_move_set) > 0:
+            ret_move = np.random.choice(list(fallback_move_set))
+            if self.verbose: print('Playing random action %s from %s' % (ret_move, fallback_move_set))
+        else:
+            raise Exception('No valid moves remaining: %s' % game.stringRepresentation(board))
+
+        return ret_move
